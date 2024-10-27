c55d5cdbd41c2f23abd2df5b2c8520ac2754d05e
ark948 <armankarimi948@gmail.com>
2024-10-27 22:35:50 +0330

    refactoring: tags and prefixes

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -4,18 +4,21 @@ from typing import List
 import database, models, schemas
 
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/blogs',
+    tags=['Blogs']
+)
 get_db = database.get_db
 
 
-@router.get('/blog', response_model=List[schemas.ShowBlog], tags=['blogs'])
+@router.get('/', response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(database.get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
 # IMPORTANT notice schema is used for validation and models for actual creation of data
-@router.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
+@router.post('/', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
     db.add(new_blog)
@@ -24,7 +27,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@router.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog, tags=['blogs'])
+@router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -32,7 +35,7 @@ def show(id, response: Response, db: Session = Depends(get_db)):
     return blog
 
 
-@router.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['blogs'])
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -42,7 +45,7 @@ def destroy(id, db: Session = Depends(get_db)):
     return 'done'
 
 
-@router.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -4,15 +4,17 @@ import database, schemas, models
 from sqlalchemy.orm import Session
 from hashing import Hash
 
-router = APIRouter()
 
+router = APIRouter(
+    prefix='/user',
+    tags=['Users']
+)
 get_db = database.get_db
-
 
 
 pwd_cxt = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-@router.post('/user', response_model=schemas.ShowUser, tags=['users'])
+@router.post('/', response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(database.get_db)):
     hashedPassword = pwd_cxt.hash(request.password)
     new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
@@ -22,7 +24,7 @@ def create_user(request: schemas.User, db: Session = Depends(database.get_db)):
     return new_user
 
 
-@router.get('/user/{id}', response_model=schemas.ShowUser, tags=['users'])
+@router.get('/{id}', response_model=schemas.ShowUser)
 def get_user(id: int, db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
