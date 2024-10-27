e4716677ace44f46f305c0a3885933160fd1c6c2
ark948 <armankarimi948@gmail.com>
2024-10-27 21:10:44 +0330

    Relationships between models (user_id is hardcoded for now)

--- blog/main.py
+++ blog/main.py
@@ -30,7 +30,7 @@ def all(db: Session = Depends(get_db)):
 # IMPORTANT notice schema is used for validation and models for actual creation of data
 @app.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
+    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,14 +1,21 @@
 from pydantic import BaseModel
+from typing import List
 
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
 
 
-class ShowBlog(BaseModel):
-    title: str
-    body: str
+class Blog(BlogBase):
+    class Config:
+        orm_mode = True
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog] = []
     class Config:
         orm_mode = True
 
@@ -18,6 +25,10 @@ class User(BaseModel):
     email: str
     password: str
 
-class ShowUser(BaseModel):
-    name: str
-    email: str
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
+    class Config:
+        orm_mode = True
