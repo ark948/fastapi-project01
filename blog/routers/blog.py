260e2f6a22969f57317a1a4cfe08af28f2e2971a
ark948 <armankarimi948@gmail.com>
2024-10-27 23:17:11 +0330

    more refactoring: crud actions in repository folder

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
+from repository import blog
 import database, models, schemas
 
 
@@ -8,48 +9,31 @@ router = APIRouter(
     prefix='/blogs',
     tags=['Blogs']
 )
+
 get_db = database.get_db
 
 
 @router.get('/', response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(database.get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+    return blog.get_all(db)
 
 
 # IMPORTANT notice schema is used for validation and models for actual creation of data
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return blog.create(request, db)
 
 
 @router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
-def show(id, response: Response, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={'details': f'Blog with {id} was not found.'})
-    return blog
+def show(id: int, response: Response, db: Session = Depends(get_db)):
+    return blog.show(id, db)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with {id} not found.")
-    blog.delete(synchronize_session=False)
-    db.commit()
-    return 'done'
+def destroy(id: int, db: Session = Depends(get_db)):
+    return blog.destroy(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with {id} not found.")
-    blog.update(request)
-    db.commit()
-    return 'updated'
+    return blog.update(id, request, db)
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from passlib.context import CryptContext
 import database, schemas, models
 from sqlalchemy.orm import Session
-from hashing import Hash
+from repository import user
 
 
 router = APIRouter(
@@ -12,21 +11,11 @@ router = APIRouter(
 get_db = database.get_db
 
 
-pwd_cxt = CryptContext(schemes=['bcrypt'], deprecated='auto')
-
 @router.post('/', response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(database.get_db)):
-    hashedPassword = pwd_cxt.hash(request.password)
-    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+    return user.create(request, db)
 
 
 @router.get('/{id}', response_model=schemas.ShowUser)
 def get_user(id: int, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with {id} was not found.")
-    return user
+    return user.show(id, db)
