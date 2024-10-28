1f60e4d116a712c724f233a357c52a9c99797baf
ark948 <armankarimi948@gmail.com>
2024-10-28 19:12:42 +0330

    Added auth reqquired to blog routes

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from tokens import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from datetime import timedelta
 from database import get_db
@@ -14,7 +15,7 @@ router = APIRouter(
 
 
 @router.post('/login')
-def login(request: schemas.Login, db: Session = Depends(get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials.")
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from repository import blog
 import database, models, schemas
+from oauth2 import get_current_user
 
 
 router = APIRouter(
@@ -14,26 +15,26 @@ get_db = database.get_db
 
 
 @router.get('/', response_model=List[schemas.ShowBlog])
-def all(db: Session = Depends(database.get_db)):
+def all(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
     return blog.get_all(db)
 
 
 # IMPORTANT notice schema is used for validation and models for actual creation of data
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+def create(request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
     return blog.create(request, db)
 
 
 @router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
-def show(id: int, response: Response, db: Session = Depends(get_db)):
+def show(id: int, response: Response, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
     return blog.show(id, db)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int, db: Session = Depends(get_db)):
+def destroy(id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
     return blog.destroy(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+def update(id, request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
     return blog.update(id, request, db)
