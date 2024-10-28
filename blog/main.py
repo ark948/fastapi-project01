53fabdda4885d56b6c3f903aa627f94c643e9ec8
ark948 <armankarimi948@gmail.com>
2024-10-28 19:38:28 +0330

    Renamed authentication router to auth

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from routers import blog, user, authentication
+from routers import blog, user, auth
 
 
 app = FastAPI()
@@ -9,7 +9,7 @@ app = FastAPI()
 models.Base.metadata.create_all(engine)
 
 
-app.include_router(authentication.router)
+app.include_router(auth.router)
 app.include_router(blog.router)
 app.include_router(user.router)
 
