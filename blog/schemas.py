cc89fb4d302b41f27550ff6bdeb7d89325d77e3e
ark948 <armankarimi948@gmail.com>
2024-10-28 18:34:58 +0330

    authentication using jwt

--- blog/schemas.py
+++ blog/schemas.py
@@ -32,3 +32,19 @@ class ShowBlog(BaseModel):
     creator: ShowUser
     class Config:
         orm_mode = True
+
+
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
