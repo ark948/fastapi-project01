eb5c1c41cb495645885e9f5dc05029211b13fe92
ark948 <armankarimi948@gmail.com>
2024-10-29 16:10:12 +0330

    minor update

--- blog/oauth2.py
+++ blog/oauth2.py
@@ -1,12 +1,13 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from blog.tokens import verify_token
+from typing import Annotated
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
