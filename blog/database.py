be093ad2b1ddbefcb5c98cb387d1dc29deb53e65
ark948 <armankarimi948@gmail.com>
2024-10-29 16:19:41 +0330

    Added env vars (secret key was exposed)

--- blog/database.py
+++ blog/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+from blog import config
 
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///blog.db'
+SQLALCHEMY_DATABASE_URL = config.DATABASE_URL
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
