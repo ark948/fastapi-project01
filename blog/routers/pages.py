142b1f71fb7f31e8c7bf2c41098f446d661f1934
ark948 <armankarimi948@gmail.com>
2024-10-29 15:26:57 +0330

    Added html templates

--- blog/routers/pages.py
+++ blog/routers/pages.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, HTTPException, Request
+from fastapi import APIRouter, status, HTTPException, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -9,3 +9,13 @@ router = APIRouter(
     prefix='/pages',
     tags=['Pages']
 )
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get('/index', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(
+        request=request, name='index.html', context={'message': "WORLD"}
+    )
